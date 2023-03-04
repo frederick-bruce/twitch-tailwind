290365175ac4242cfcc0cb3382acf81542d6e13a
Frederick Bruce <frederick.bruce.dev@gmail.com>
2023-03-04 05:55:28 -0500

    update

--- components/LiveChannels.jsx
+++ components/LiveChannels.jsx
@@ -15,7 +15,7 @@ const LiveChannels = () => {
   return (
     <div id='live' className='p-2 md:p-8'>
       <h2 className='text-xl font-bold px-2'>
-        <span className='text-[#9147ff]'>Live Channels</span> we think you&apos;ll
+        <span className='text-[#9147ff]'>Live Channels</span> we thank you&apos;ll
         like
       </h2>
       {/* Container */}
